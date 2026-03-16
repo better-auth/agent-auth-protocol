4cf2ba0b23a35fbff9cda6d33e52f6dff16c06d6
Taesu <bytaesu@gmail.com>
2026-03-17 01:14:35 +0900

    style: reduce halftone background intensity in light mode

--- components/ui/halftone-background.tsx
+++ components/ui/halftone-background.tsx
@@ -178,7 +178,7 @@ export function HalftoneBackground() {
 			className="absolute inset-0 overflow-hidden bg-white dark:bg-black"
 			aria-hidden="true"
 		>
-			<canvas ref={canvasRef} className="w-full h-full invert dark:invert-0" />
+			<canvas ref={canvasRef} className="w-full h-full invert opacity-60 dark:invert-0 dark:opacity-100" />
 		</div>
 	);
 }
